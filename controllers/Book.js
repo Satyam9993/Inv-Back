5e4862e99e7700c5509ac14fc0eeef761525b4a2
Satyam Katiyar <satyamkatiyar9993@gmail.com>
2023-05-26 12:29:53 +0530

    commit

--- controllers/Book.js
+++ controllers/Book.js
@@ -2,7 +2,7 @@ const inventory = require('../models/Inventory');
 
 exports.createInv = async (req, res) => {
     try {
-        await inventory.create({...req.body, userId: req.user.id})
+        await inventory.create({ ...req.body, userId: req.user.id })
             .then((inv) => {
                 res.send({ inv: inv });
             })
@@ -13,14 +13,13 @@ exports.createInv = async (req, res) => {
     } catch (error) {
         res.status(501).send({ err: "Server error" });
     }
-}
-
+};
 
 exports.fetchInv = async (req, res) => {
     try {
         await inventory.find()
             .then((invs) => {
-                res.send({ invs : invs });
+                res.send({ invs: invs });
             })
             .catch(err => {
                 res.status(500).send({ err });
@@ -28,5 +27,56 @@ exports.fetchInv = async (req, res) => {
 
     } catch (error) {
         res.status(501).send({ err: "Server error" });
+    }
+};
+
+exports.updateInv = async (req, res) => {
+    try {
+        await inventory.findByIdAndUpdate({ _id: req.params.invId },
+            {
+                $set: {
+                    itemName: req.body.itemName,
+                    image: req.body.image,
+                    category: req.body.category,
+                    code: req.body.code,
+                    description: req.body.description,
+                    inclusivetax: req.body.inclusivetax,
+                    lowstockunit: req.body.lowstockunit,
+                    openstock: req.body.openstock,
+                    purchaseprice: req.body.purchaseprice,
+                    unit: req.body.unit,
+                    stockwarning: req.body.stockwarning,
+                    taxrate: req.body.taxrate
+                }
+            })
+            .then(async (inv) => {
+                await inventory.findById({ _id: inv._id }).then((inventory) => {
+                    res.send({ inv: inventory });
+                }).catch((err) => {
+                    res.status(502).send({ err });
+                });
+            })
+            .catch(err => {
+                res.status(500).send({ err });
+            })
+
+    } catch (error) {
+        res.status(501).send({ err: "Server error" });
+    }
+};
+
+exports.deleteInv = async (req, res) => {
+    try {
+        const { ids } = req.body;
+        const deletedInventory = await inventory.deleteMany({ _id: { $in: ids } });
+
+        if (deletedInventory.deletedCount === 0) {
+            return res.status(404).json({ error: 'No inventories found' });
+        }
+
+        res.status(200).json({ message: 'Inventories deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 };
--- routes/Book.js
+++ routes/Book.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 const {
     createInv,
-    fetchInv
+    fetchInv,
+    updateInv,
+    deleteInv
 } = require('../controllers/Book');
 const fetchuser = require('../middleware/fetchuser')
 
 router.post('/',fetchuser,createInv);
 router.get('/',fetchuser,fetchInv);
+router.put('/:invId',fetchuser,updateInv);
+router.delete('/',fetchuser,deleteInv);
 
 module.exports = router;
